8be10201b728158923ac6ba80b147c325993270b
Trần Tùng Lâm <62226612+Louis2602@users.noreply.github.com>
2024-11-25 22:23:59 +0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,49 +1,152 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+local function file_exists(path)
+	local f = io.open(path, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
 
--- This is where you actually apply your config choices
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
 
--- For example, changing the color scheme:
-config.colors = {
-	foreground = "#FFFFFF",
-	background = "#000000",
-	cursor_bg = "#00C4FF",
-	cursor_border = "#00C4FF",
-	cursor_fg = "#011423",
-	selection_bg = "#C1DEFF",
-	selection_fg = "#FFFFFF",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- #1CCFB8
+-- #DFEDEE
+
+local config = {
+	colors = {
+		background = "#1C1F20",
+		cursor_border = "#DFEDEE",
+		cursor_fg = "#181818",
+		cursor_bg = "#DFEDEE",
+	},
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium", italic = false }),
+	cursor_thickness = "1px",
+	enable_tab_bar = true,
+	window_decorations = "RESIZE",
+	window_background_opacity = 1.0,
+	window_close_confirmation = "NeverPrompt",
+	tab_bar_at_bottom = true,
+	show_new_tab_button_in_tab_bar = false,
+	use_fancy_tab_bar = true,
+	adjust_window_size_when_changing_font_size = false,
+
+	audible_bell = "Disabled",
+	check_for_updates = false,
+	color_scheme = "Obsidian",
+	inactive_pane_hsb = {
+		hue = 1.0,
+		saturation = 1.0,
+		brightness = 1.0,
+	},
+	font_size = 16.0,
+	launch_menu = {},
+	leader = { key = " ", mods = "CTRL" },
+	disable_default_key_bindings = true,
+	keys = {
+		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+		{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+		{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+		{
+			key = "\\",
+			mods = "LEADER",
+			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+		},
+		{
+			key = "w",
+			mods = "LEADER",
+			action = wezterm.action.CloseCurrentTab({ confirm = false }),
+		},
+		{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+		{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+		{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+		{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+		{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+		{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+
+		{ key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
+		{ key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+		{ key = "c", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },
+		{ key = "+", mods = "SHIFT|CTRL", action = "IncreaseFontSize" },
+		{ key = "-", mods = "CTRL|ALT", action = "DecreaseFontSize" },
+		{ key = "0", mods = "SHIFT|CTRL", action = "ResetFontSize" },
+	},
+	set_environment_variables = {},
+	default_prog = { "D:\\Git\\bin\\bash.exe", "-l" },
 }
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium", italic = false })
-config.font_size = 14
-config.cursor_thickness = "1px"
-config.enable_tab_bar = false
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- config.front_end = "Software" -- OpenGL doesn't work quite well with RDP.
+	-- config.term = "" -- Set to empty so FZF works on windows
+	table.insert(config.launch_menu, { label = "PowerShell", args = { "powershell.exe", "-NoLogo" } })
 
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 8
+	-- Find installed visual studio version(s) and add their compilation
+	-- environment command prompts to the menu
+	for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+		local year = vsvers:gsub("Microsoft Visual Studio/", "")
+		table.insert(config.launch_menu, {
+			label = "x64 Native Tools VS " .. year,
+			args = {
+				"cmd.exe",
+				"/k",
+				"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
+			},
+		})
+	end
+else
+	local fish_bin_path = "/bin/fish"
+	if file_exists("/opt/homebrew/bin/fish") then
+		fish_bin_path = "/opt/homebrew/bin/fish"
+		config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+	else
+		config.default_prog = { "/bin/bash", "-l" }
+	end
+	table.insert(config.launch_menu, { label = "fish", args = { fish_bin_path, "-l" } })
+	table.insert(config.launch_menu, { label = "bash", args = { "bash", "-l" } })
+end
 
--- Spawn a bash shell in login mode
-config.default_prog = { "D:\\Git\\bin\\bash.exe", "-l" }
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
 
-config.keys = {
-	-- This will create a new split and run your default program inside it
-	{
-		key = "+",
-		mods = "ALT|SHIFT",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = '"',
-		mods = "ALT|SHIFT",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-}
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab_title(tab)
+	local truncated_title = wezterm.truncate_right(title, max_width)
 
--- and finally, return the configuration to wezterm
+	if tab.is_active then
+		return {
+			{ Text = " " .. truncated_title .. " " },
+		}
+	end
+	return truncated_title
+end)
+
 return config
